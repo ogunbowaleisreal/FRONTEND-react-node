d105db8d08b12aeac1b841ef2de3418f67bdd98b
ogunbowaleisreal <ogunbowaleisreal4@gmail.com>
2025-05-16 11:31:05 +0100

    Update Home.jsx

--- react-todo/src/pages/Home.jsx
+++ react-todo/src/pages/Home.jsx
@@ -1,8 +1,22 @@
-
+import api from "../api"
+import { useState , useEffect } from "react";
 function Home(){
+    const [todolist , settodos] = useState('')
+
+    const gettodos = async()=>{
+        try{
+        const todos = api.get('/')
+        if(todos){
+
+        }
+    }catch(err){
+        
+    }
+    }
     return (
         <div>
             <p>Home</p>
+
         </div>
     )
 }
